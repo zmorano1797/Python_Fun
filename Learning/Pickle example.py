58a22ee0b9d2e5fb8571c78e8fb22fcd7d4f5e7e
zmorano1797 <zmorano1797@outlook.com>
2024-12-13 21:06:46 -0500

    commit

--- Learning/Pickle example.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-f = open('store.pckl', 'rb')
-obj = pickle.load(f)
-f.close()
-
-f = open('store.pckl', 'wb')
-pickle.dump(obj, f)
-f.close()
--- Learning/Starting out.py
+++
@@ -1,6 +0,0 @@
-import easygui as g
-import commands as d
-
-#g.msgbox("Adventures of Larry", title="AoL", ok_button="Continue")
-
-d.CharName()
